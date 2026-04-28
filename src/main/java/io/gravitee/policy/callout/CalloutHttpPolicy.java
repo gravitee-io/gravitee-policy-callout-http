2276e65779b9d9e2eafa1a307a0d7fa33c7a3184
Sami CHEHADE <sami.chehade@graviteesource.com>
2026-04-28 12:14:44 +0200

    fix: performance improvement : make http connection pool configurable for http callout policy
    https://gravitee.atlassian.net/browse/APIM-13322

--- src/main/java/io/gravitee/policy/callout/CalloutHttpPolicy.java
+++ src/main/java/io/gravitee/policy/callout/CalloutHttpPolicy.java
@@ -232,14 +232,18 @@ public class CalloutHttpPolicy extends CalloutHttpPolicyV3 implements HttpPolicy
      * @param ctx The context to get Vertx component
      * @return Built or existing HttpClient
      */
-    private HttpClient getHttpClient(BaseExecutionContext ctx) {
+    HttpClient getHttpClient(BaseExecutionContext ctx) {
         if (this.httpClient == null) {
             synchronized (this) {
                 if (this.httpClient == null) {
                     // SSL options (trustAll, verifyHost) are set at client level but only applied per-request
                     // when the URL scheme is HTTPS. Vertx's RequestOptions.setAbsoluteURI() overrides the client
                     // SSL setting based on the actual URL scheme, so HTTP requests are not affected.
-                    var options = new HttpClientOptions().setSsl(true).setTrustAll(true).setVerifyHost(false);
+                    var options = new HttpClientOptions()
+                        .setSsl(true)
+                        .setTrustAll(true)
+                        .setVerifyHost(false)
+                        .setMaxPoolSize(configuration.getHttpOptions().getMaxConcurrentConnections());
 
                     if (configuration.isUseSystemProxy()) {
                         Configuration config = ctx.getComponent(Configuration.class);
